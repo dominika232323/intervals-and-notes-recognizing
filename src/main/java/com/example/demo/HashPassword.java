d4edd35efb1c9c60576a4c86d980abd24cb9f6c1
dominika232323 <dominikaboguszewska62@gmail.com>
2023-12-28 20:20:14 +0100

    Add generateSalt function

--- src/main/java/com/example/demo/HashPassword.java
+++ src/main/java/com/example/demo/HashPassword.java
@@ -1,8 +1,17 @@
 package com.example.demo;
 
+import java.security.SecureRandom;
+
 public class HashPassword {
     static public String hashPassword(String password) {
         return "";
+    }
+
+    public static byte[] generateSalt() {
+        byte[] salt = new byte[16];
+        SecureRandom random = new SecureRandom();
+        random.nextBytes(salt);
+        return salt;
     }
 
     static public boolean checkPasswordCorrectness(String password, String hash) {
