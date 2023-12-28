3580d5a2f02f2ecccf9167e7f59508c5eb4f66d2
dominika232323 <dominikaboguszewska62@gmail.com>
2023-12-28 20:27:30 +0100

    Add bytesToString

--- src/main/java/com/example/demo/HashPassword.java
+++ src/main/java/com/example/demo/HashPassword.java
@@ -7,7 +7,11 @@ public class HashPassword {
         return "";
     }
 
-    public static byte[] generateSalt() {
+    public static String generateSalt() {
+        return bytesToString(generateSaltBytes());
+    }
+
+    private static byte[] generateSaltBytes() {
         byte[] salt = new byte[16];
         SecureRandom random = new SecureRandom();
         random.nextBytes(salt);
@@ -17,5 +21,15 @@ public class HashPassword {
     static public boolean checkPasswordCorrectness(String password, String hash) {
         String passwordHash = hashPassword(password);
         return passwordHash.equals(hash);
+    }
+
+    static private String bytesToString(byte[] bytes) {
+        StringBuilder sb = new StringBuilder();
+
+        for (byte b : bytes) {
+            sb.append(String.format("%02x", b));
+        }
+
+        return sb.toString();
     }
 }
