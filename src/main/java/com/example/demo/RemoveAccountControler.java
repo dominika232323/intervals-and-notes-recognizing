d4cc26d6251b341b36bedff38ef6d570b7c95db1
dominika232323 <dominikaboguszewska62@gmail.com>
2023-12-30 17:34:38 +0100

    Add removing user

--- src/main/java/com/example/demo/RemoveAccountControler.java
+++ src/main/java/com/example/demo/RemoveAccountControler.java
@@ -7,8 +7,14 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import org.jooq.DSLContext;
+import org.jooq.Record;
+import org.jooq.Result;
 
 import java.io.IOException;
+import java.sql.SQLException;
+
+import static com.example.demo.jooq.tables.Users.USERS;
 
 public class RemoveAccountControler {
     @FXML
@@ -34,5 +40,36 @@ public class RemoveAccountControler {
     public void onExitAppButton()
     {
         Platform.exit();
+    }
+
+    @FXML
+    public void onRemoveAccountButton(ActionEvent event) throws IOException, SQLException {
+        DSLContext create = SharedFunctionsController.getDLCContex();
+
+        String login = enterLogin.getText();
+        String password = enterPassword.getText();
+
+        Result<Record> userInfo = UserTableOperations.getUserRecordByLogin(login, create);
+
+        if (UserTableOperations.checkIfLoginExists(userInfo)) {
+            Record r = userInfo.get(0);
+
+            int userID = r.get(USERS.USERID);
+            String userName = r.get(USERS.NAME);
+            String userHash = r.get(USERS.PASSWORDHASH);
+
+            String passwordHash = HashPassword.hashPassword(password);
+
+            if (passwordHash.equals(userHash)) {
+                UserTableOperations.deleteUser(userID, create);
+                incorrectLoginPassword.setText("Konto " + login + " zostało usunięte.");
+            }
+            else {
+                incorrectLoginPassword.setText("Niepoprawne hasło!!!");
+            }
+        }
+        else {
+            incorrectLoginPassword.setText("Konto " + login + " nie istnieje!!!");
+        }
     }
 }
