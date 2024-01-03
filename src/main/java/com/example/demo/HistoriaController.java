e8f121d82903a992b779af9f15a2d963d079c582
dominika232323 <dominikaboguszewska62@gmail.com>
2024-01-03 17:53:03 +0100

    Modify history-view and add fxml iteams to historiacontroller

--- src/main/java/com/example/demo/HistoriaController.java
+++ src/main/java/com/example/demo/HistoriaController.java
@@ -2,25 +2,42 @@ package com.example.demo;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
 
 import java.io.IOException;
 
 public class HistoriaController {
     @FXML
     private Label historyLabel;
-
-//    @FXML
-//    protected void initialize() {
-//        if (historyLabel != null) {
-//            historyLabel.setText("Historia gier");
-//        }
-//    }
+    @FXML
+    private Button backToMenuButton;
+    @FXML
+    private Button showResultsButton;
+    @FXML
+    private CheckBox notesGameCheckBox;
+    @FXML
+    private CheckBox intervalGameCheckBox;
+    @FXML
+    private CheckBox chooseLevelCheckBox;
+    @FXML
+    private CheckBox chooseAnswersCorrectnessCheckBox;
+    @FXML
+    private CheckBox chooseEverythingCheckBox;
+    @FXML
+    private CheckBox chooseNothingCheckBox;
+    @FXML
+    private DatePicker firstDatePicker;
+    @FXML
+    private DatePicker secondDatePicker;
+    @FXML
+    private ChoiceBox chosenLevelChoiceBox;
+    @FXML
+    private ChoiceBox chosenAnswersCorrectnessChoiceBox;
+    @FXML ScrollPane resultsScrollPane;
 
     @FXML
     private void onBackToMenuButtonClick(ActionEvent event) throws IOException {
         SharedFunctionsController menuButton = new SharedFunctionsController();
         menuButton.onBackToMenuButtonClick(event);
     }
-    // Add other methods for handling actions in the "stats_view.fxml" scene
 }
