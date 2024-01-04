dd7e8b7a285af5658da89e2a6a70afa3df64aec1
dominika232323 <dominikaboguszewska62@gmail.com>
2024-01-05 00:46:46 +0100

    Add getting games methods

--- src/main/java/com/example/demo/GameHistoryTablesOperations.java
+++ src/main/java/com/example/demo/GameHistoryTablesOperations.java
@@ -6,6 +6,11 @@ import org.jooq.Result;
 
 import static com.example.demo.jooq.tables.Levelnotes.LEVELNOTES;
 import static com.example.demo.jooq.tables.Levelintervals.LEVELINTERVALS;
+import static com.example.demo.jooq.tables.Notesgames.NOTESGAMES;
+import static com.example.demo.jooq.tables.Answersnotesgame.ANSWERSNOTESGAME;
+import static com.example.demo.jooq.tables.Intervalsgame.INTERVALSGAME;
+import static com.example.demo.jooq.tables.Answersintervalsgame.ANSWERSINTERVALSGAME;
+
 
 public class GameHistoryTablesOperations {
     static public Result<Record> getNotesLevelsByUserID(int id, DSLContext create) {
@@ -20,5 +25,26 @@ public class GameHistoryTablesOperations {
                 .from(LEVELINTERVALS)
                 .where(LEVELINTERVALS.USERID.eq(id))
                 .fetch();
+    }
+
+    static public Result<Record> getNotesGamesByUserID(int id, DSLContext create) {
+        return create.select()
+                .from(NOTESGAMES)
+                .where(NOTESGAMES.USERID.eq(id))
+                .fetch();
+    }
+
+    static public Result<Record> getIntervalGamesByUserID(int id, DSLContext create) {
+        return create.select()
+                .from(INTERVALSGAME)
+                .where(INTERVALSGAME.USERID.eq(id))
+                .fetch();
+    }
+
+    static public Result<Record> getAllGamesByUserID(int id, DSLContext create) {
+        Result<Record> allGames = getNotesGamesByUserID(id, create);
+        allGames.addAll(getIntervalGamesByUserID(id, create));
+
+        return allGames;
     }
 }
