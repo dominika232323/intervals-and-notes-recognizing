346df468dfcf7b5a51b003222dd35fff156b5e4c
Michal <michal.golawski.mg@gmail.com>
2024-01-05 00:19:29 +0100

    Implemented new methods to randomly choose interval and randomly choose notes that will be used to represent chosen interval

--- src/main/java/com/example/demo/IntervalsGameController.java
+++ src/main/java/com/example/demo/IntervalsGameController.java
@@ -1,9 +1,20 @@
 package com.example.demo;
+import com.example.demo.jooq.Tables;
+import com.example.demo.jooq.tables.records.IntervalsRecord;
+import com.example.demo.jooq.tables.records.NotesRecord;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import org.jooq.DSLContext;
+import org.jooq.SQLDialect;
+import org.jooq.impl.DSL;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Random;
 
 public class IntervalsGameController {
     @FXML
@@ -15,9 +26,13 @@ public class IntervalsGameController {
     @FXML
     private Label correct_answers_label;
 
+    private List<IntervalsRecord> allIntervalsList;
+    private List<NotesRecord> allNotesList;
+
     @FXML
     void intervalChosenOnClick(ActionEvent event) {
-        System.out.println("eeee");
+        Button button = (Button) event.getSource();
+        System.out.println(button.getText());
     }
 
     @FXML
@@ -30,4 +45,35 @@ public class IntervalsGameController {
     void exitAndSaveOnClick(ActionEvent event) {
 
     }
+
+    public void initialize() throws SQLException {
+        // getting connection with database
+        Connection connection = DatabaseConnection.getInstance().getConnection();
+        DSLContext create = DSL.using(connection, SQLDialect.MYSQL);
+
+        // initializing allIntervalsList and allNotesList
+        allIntervalsList = create.selectFrom(Tables.INTERVALS).fetch();
+        allNotesList = create.selectFrom(Tables.NOTES).fetch();
+
+    }
+
+    private IntervalsRecord chooseRandomInterval(){
+        Random random = new Random();
+        int randomIndex = random.nextInt(allIntervalsList.size());
+        return allIntervalsList.get(randomIndex);
+    }
+
+    private NotesRecord getRandomHigherNoteFromInterval(IntervalsRecord interval){
+        int semitones = interval.getIntervalid() - 1;
+        Random random = new Random();
+        int randomIndex = random.nextInt(allIntervalsList.size() - semitones) + semitones;
+
+        return allNotesList.get(randomIndex);
+    }
+
+    private NotesRecord getLowerNoteFromHigherNote(IntervalsRecord interval, NotesRecord highNote){
+        int semitones = interval.getIntervalid() - 1;
+        return allNotesList.get(highNote.getNoteid() - 1 - semitones);
+    }
+
 }
