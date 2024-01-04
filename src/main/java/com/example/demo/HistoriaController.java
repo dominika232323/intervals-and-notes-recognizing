b34ba2f9bbc27660082e1d3e1e03c9cabfda0daf
dominika232323 <dominikaboguszewska62@gmail.com>
2024-01-04 13:09:04 +0100

    Add fillChoiceBoxWithAnswersCorrectness

--- src/main/java/com/example/demo/HistoriaController.java
+++ src/main/java/com/example/demo/HistoriaController.java
@@ -102,6 +102,23 @@ public class HistoriaController implements Initializable {
         }
     }
 
+    @FXML void fillChoiceBoxWithAnswersCorrectness(ActionEvent event) {
+        ArrayList<String> answersCorrectness = new ArrayList<String>();
+
+        int lower_bound = 0;
+        int upper_bound = 10;
+
+        while (upper_bound < 110) {
+            String correctness = lower_bound + "% - " + upper_bound + "%";
+            answersCorrectness.add(correctness);
+
+            lower_bound = lower_bound + 10;
+            upper_bound = upper_bound + 10;
+        }
+
+        chosenAnswersCorrectnessChoiceBox.setItems(FXCollections.observableArrayList(answersCorrectness));
+    }
+
     @FXML
     public void fillChoiceBoxWithLevels(ActionEvent event) throws SQLException {
         DSLContext create = SharedFunctionsController.getDLCContex();
